110838134f2b0aecdfdff4434e9ebd05089a7657
Jared Nolen <Legodoghead@gmail.com>
2022-11-14 17:27:54 -0800

    Basic card layout
    - Unit SO to hold unit card data
    - Card class
    - Card Unit derrived class
    - - Performs setup, (Reads SO)
    - Unit card UI prefab

---
+++ UphillBattleCG/Assets/Scripts/UnitSO.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CardSO/Unit")]
+public class UnitSO : ScriptableObject
+{
+    public int cost;
+    public int health;
+    public int damage;
+    public int armor;
+
+    public string title;
+    public string description;
+
+    public enum Position
+    {
+        Frontline,
+        Backline,
+        Versatile
+    } 
+    public Position position;
+
+    public Sprite art;
+    public Vector2 cardArtOffset;
+}
